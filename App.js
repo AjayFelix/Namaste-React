a7dd3085db76e2d8117b7edca88791dd244bd412
ajayfelix007@gmail.com <ajayfelix007@gmail.com>
2024-01-03 15:16:09 +0530

    episode-1(Inception)

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
-const heading = React.createElement("h1", {}, "hello from react!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello from react!"
+);
 //1st props is for tag type , 2nd for atribute id or name 2rd props is for component
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
