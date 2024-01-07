01d0e96db7abe6621bc98b86ef7fc85780879e75
ajayfelix007@gmail.com <ajayfelix007@gmail.com>
2024-01-07 20:56:31 +0530

    ep-3 setting up the app to start project

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "hello from react!"
-);
+const Title = () => {
+  return (
+    <div>
+      <h1>Namaste React</h1>
+    </div>
+  );
+};
+
+// react components
+// class based components - old way
+// function based components - present way
+
+//component compositions
+const Heading = () => {
+  return (
+    <div>
+      <Title />
+      <h1 className="heading">Hi ajay felix wecome to react</h1>
+    </div>
+  );
+};
+
 //1st props is for tag type , 2nd for atribute id or name 2rd props is for component
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />);
